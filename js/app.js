4474c4d442772722de7aaa680e77c86a13449130
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-09-30 20:22:17 -0600

    Got it!!!!

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const progress = document.querySelector('.progress');
+
+let n = 0;
+const percent = ['0%','33%','66%','99%']
+
+document.querySelector('#Next').addEventListener('click', () => {
+    if(n !== 3) {
+        document.querySelector('#Prev').disabled = false;
+        progress.style.width = percent[n += 1]
+    } 
+    if(n === 3) {
+        document.querySelector('#Next').disabled = true;
+    }
+})
+document.querySelector('#Prev').addEventListener('click', () => {
+    if(n !== 0) {
+        document.querySelector('#Next').disabled = false;
+        progress.style.width = percent[n -= 1];
+    } 
+    if(n === 0) {
+        document.querySelector('#Prev').disabled = true;
+    }
+})
